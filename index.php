dce144259a01dcfde91c3a687eed489e03c87f2f
Yannick Lohse <yannick.lohse@caldera.com>
2013-10-02 15:35:18 +0200

    Footer

--- index.php
+++ index.php
@@ -242,28 +242,4 @@
 	</div>
 </section>
 
-<section class="home-sidebar">
-	<div class="wrapper">
-		<div>
-			
-		</div>
-		<div class="module-newsletter">
-			<h3>
-				Newsletter
-			</h3>
-			<p>
-				Abonnez-vous à notre newsletter 
-et recevez des astuces tous les jeudi matins.
-			</p>
-			<form>
-				<span class="form-email">
-					<input type="email" />
-				</span>
-				<span class="form-submit">
-					<input type="submit" value="Recevoir les news" />
-				</span>
-			</form>
-		</div>
-	</div>
-</section>
 <?php get_footer(); ?>
