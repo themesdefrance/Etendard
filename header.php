736aedcb8a2e622ba732d5c5983547349ca178a1
Yannick Lohse <yannick.lohse@caldera.com>
2013-10-02 15:33:33 +0200

    header

--- header.php
+++ header.php
@@ -1,46 +1,34 @@
 <!doctype html>
-<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="lt-ie9" lang="en"> <![endif]-->
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>    <html class="lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>    <html class="lt-ie9" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 8]><!-->
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
-	<meta charset="utf-8">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	
-	<title></title>
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<meta name="description" content="">
-	<link rel="shortcut icon" href="/favicon.ico?v=0">
+<!--	<link rel="shortcut icon" href="/favicon.ico?v=0">-->
 	
 	<meta name="viewport" content="width=device-width">
-	
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css?v=0">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/fonts/style.css">
-	<link href='http://fonts.googleapis.com/css?family=Sanchez:400,400italic|Maven+Pro:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 	
 	<!-- Candy -->
 	<link rel="dns-prefetch" href="//ajax.googleapis.com" />
 	
-	<!-- RSS 
-	<link rel="alternate" type="application/rss+xml" title="RSS" href="/rss.xml" />
-	  -->
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/fonts/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Sanchez:400,400italic|Maven+Pro:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css?v=0">
+	
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	
 	<!-- Scripts that need to be loaded first -->
 	<!--[if lt IE 9]>
 	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-	<!-- 
-	<script>
-		var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
-		(function(d, t){
-			var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
-			g.src = ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js';
-			s.parentNode.insertBefore(g, s)
-		}(document, 'script'));
-	</script>
-	-->
 </head>
-<body>
+<body <?php body_class(); ?>>
 <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
