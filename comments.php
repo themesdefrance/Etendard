d31bce38016434bbd38ad9be9d763a292e83d486
Yannick Lohse <glorf.travian@gmail.com>
2013-10-05 14:35:51 +0200

    Commentaires propres

--- comments.php
+++ comments.php
@@ -4,25 +4,26 @@
 	<?php if (have_comments()) : ?>
 		<h2 class="comments-title">
 			<?php
-                printf(_n('One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'shape'),
-                    	number_format_i18n( get_comments_number() ), '<span>'.get_the_title().'</span>');
+                printf(_n('Une personne à écrit un commentaire, et vous ?', '%1$s personnes ont déja écrit un commentaire, et vous ?', get_comments_number(), TEXT_TRANSLATION_DOMAIN), number_format_i18n(get_comments_number()));
             ?>
 		</h2>
 		
-		<ol class="commentlist">
+		<?php comment_form(); ?>
+		
+		<ol class="comment-list">
 			<?php wp_list_comments(array('callback'=>'shape_comment')); ?>
 		</ol><!-- .commentlist -->
  
         <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
-		<nav role="navigation" id="comment-nav-above" class="site-navigation comment-navigation">
+		<nav role="navigation" id="comment-nav-below" class="comment-navigation">
 			<h1 class="assistive-text">
 				<?php _e('Comment navigation', 'shape'); ?>
 			</h1>
 			<div class="nav-previous">
-            	<?php previous_comments_link(__('&larr; Older Comments', 'shape')); ?>
+            	<?php previous_comments_link(__('&larr; Older Comments', TEXT_TRANSLATION_DOMAIN)); ?>
             </div>
             <div class="nav-next">
-            	<?php next_comments_link(__('Newer Comments &rarr;', 'shape')); ?>
+            	<?php next_comments_link(__('Newer Comments &rarr;', TEXT_TRANSLATION_DOMAIN)); ?>
             </div>
         </nav><!-- #comment-nav-before .site-navigation .comment-navigation -->
         <?php endif; // check for comment navigation ?>
@@ -31,9 +32,7 @@
  
     <?php if (!comments_open() && get_comments_number() != '0' && post_type_supports(get_post_type(), 'comments')) : ?>
 		<p class="nocomments">
-			<?php _e( 'Comments are closed.', 'shape' ); ?>
+			<?php _e('Comments are closed.', TEXT_TRANSLATION_DOMAIN); ?>
 		</p>
 	<?php endif; ?>
-	
-	<?php comment_form(); ?>
 </div><!-- #comments .comments-area -->
--- functions.php
+++ functions.php
@@ -23,6 +23,12 @@ register_sidebar(array(
 	
 add_theme_support('post-thumbnails');
 
+function etendard_get_avatar($avatar){
+	$avatar = preg_replace("/\s(width|height)='\d+'/", ' ', $avatar);
+	return $avatar;
+}
+add_filter('get_avatar', 'etendard_get_avatar');
+
 
 //derived from http://www.wpbeginner.com/wp-themes/how-to-add-numeric-pagination-in-your-wordpress-theme/
 /**
@@ -96,58 +102,57 @@ function etendard_posts_nav($extremes=true, $separator='|'){
 	if ($extremes && get_next_posts_link()) next_posts_link();
 }
 
-if ( ! function_exists( 'shape_comment' ) ) :
-/**
- * Template for comments and pingbacks.
- *
- * Used as a callback by wp_list_comments() for displaying the comments.
- *
- * @since Shape 1.0
- */
-function shape_comment( $comment, $args, $depth ) {
-    $GLOBALS['comment'] = $comment;
-    switch ( $comment->comment_type ) :
-        case 'pingback' :
-        case 'trackback' :
-    ?>
-    <li class="post pingback">
-        <p><?php _e( 'Pingback:', 'shape' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'shape' ), ' ' ); ?></p>
-    <?php
-            break;
-        default :
-    ?>
-    <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
-        <article id="comment-<?php comment_ID(); ?>" class="comment">
-            <footer>
-                <div class="comment-author vcard">
-                    <?php echo get_avatar( $comment, 40 ); ?>
-                    <?php printf( __( '%s <span class="says">says:</span>', 'shape' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
-                </div><!-- .comment-author .vcard -->
-                <?php if ( $comment->comment_approved == '0' ) : ?>
-                    <em><?php _e( 'Your comment is awaiting moderation.', 'shape' ); ?></em>
-                    <br />
-                <?php endif; ?>
- 
-                <div class="comment-meta commentmetadata">
-                    <a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time pubdate datetime="<?php comment_time( 'c' ); ?>">
-                    <?php
-                        /* translators: 1: date, 2: time */
-                        printf( __( '%1$s at %2$s', 'shape' ), get_comment_date(), get_comment_time() ); ?>
-                    </time></a>
-                    <?php edit_comment_link( __( '(Edit)', 'shape' ), ' ' );
-                    ?>
-                </div><!-- .comment-meta .commentmetadata -->
-            </footer>
- 
-            <div class="comment-content"><?php comment_text(); ?></div>
- 
-            <div class="reply">
-                <?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-            </div><!-- .reply -->
-        </article><!-- #comment-## -->
- 
-    <?php
-            break;
-    endswitch;
+//borrowed from http://themeshaper.com/2012/11/04/the-wordpress-theme-comments-template/
+if (!function_exists( 'shape_comment')) :
+function shape_comment($comment, $args, $depth){
+	$GLOBALS['comment'] = $comment;
+	switch ($comment->comment_type) :
+		case 'pingback' :
+		case 'trackback' :
+	?>
+	<li class="post pingback">
+		<p>
+			<?php _e('Pingback:', TEXT_TRANSLATION_DOMAIN); ?>
+			<?php comment_author_link(); ?>
+			<?php edit_comment_link(__('(Éditer)', TEXT_TRANSLATION_DOMAIN), ' '); ?>
+		</p>
+	<?php
+		break;
+	default :
+	?>
+	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+		<article id="comment-<?php comment_ID(); ?>" class="article comment">
+			<aside class="col-1-5">
+				<?php if ($comment->comment_approved == '0') : ?>
+					<em><?php _e('Your comment is awaiting moderation.', TEXT_TRANSLATION_DOMAIN); ?></em>
+				<?php endif; ?>
+				<?php echo get_avatar($comment, 104); ?>
+			</aside>
+			
+			<div class="col-4-5">
+				<header class="comment-header">
+					<div class="comment-author vcard">
+						<?php printf(__('%s', TEXT_TRANSLATION_DOMAIN), sprintf('<cite class="fn">%s</cite>', get_comment_author_link())); ?>
+						<?php edit_comment_link(__('(Éditer)', TEXT_TRANSLATION_DOMAIN), ' '); ?>
+					</div>
+				</header>
+	 
+				<div class="content">
+					<?php comment_text(); ?>
+				</div>
+				
+				<div class="reply">
+					<?php 
+					comment_reply_link(array_merge($args, 
+						array(	'depth'=>$depth, 
+								'max_depth'=>$args['max_depth'],
+								'reply_text'=>__('Répondre', TEXT_TRANSLATION_DOMAIN)))); 
+					?>
+				</div><!-- .reply -->
+			</div>
+		</article><!-- #comment-## -->
+	<?php
+		break;
+	endswitch;
 }
 endif; // ends check for shape_comment()
