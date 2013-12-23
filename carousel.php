1ae203e91dfccd0ab535c4b8e8125b45b47fad04
Yannick Lohse <glorf.travian@gmail.com>
2013-12-23 11:55:09 +0100

    Suppression d'un message d'erreur

--- carousel.php
+++ carousel.php
@@ -1,6 +1,10 @@
 <?php
+$carousel = null;
+
 $custom = get_post_custom();
-$carousel = maybe_unserialize($custom['etendard_portfolio_carousel'][0]);
+if (isset($custom['etendard_portfolio_carousel'])) 
+	$carousel = maybe_unserialize($custom['etendard_portfolio_carousel'][0]);
+	
 if (is_array($carousel) && trim($carousel[count($carousel)-1]) === '') array_pop($carousel);
 ?>
 <?php if (count($carousel) > 0): ?>
