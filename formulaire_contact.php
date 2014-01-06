50d0b837bc800a3ff706926e7ea6329d5a969e5d
Yannick Lohse <glorf.travian@gmail.com>
2014-01-06 19:41:24 +0100

    Fonctionnement contact #46

--- formulaire_contact.php
+++ formulaire_contact.php
@@ -1,8 +1,23 @@
 <?php 
-if (isset($_POST['etendard_contact_nonce'])){
-	echo 'envois le mail.';
+if (isset($_POST['etendard_contact_nonce']) && wp_verify_nonce($_POST['etendard_contact_nonce'], 'etendard_contact_nonce')){
+	$nom = $_POST['etendard_contact_nom'];
+	$email = $_POST['etendard_contact_email'];
+	$sujet = $_POST['etendard_contact_sujet'];
+	$message = $_POST['etendard_contact_message'];
+	
+	$to = get_bloginfo('admin_email');
+	
+	$subject = (trim($sujet) === '') ? __('Contact', TEXT_TRANSLATION_DOMAIN).' '.get_bloginfo('name') : $sujet;
+	$from = $nom;
+	$from_mail = (trim($sujet) === '') ? $to : $email;
+	
+	$headers = array();
+	$headers[] = 'From: '.$from.' <'.$from_mail.'>';
+	
+	$contact = wp_mail($to, $subject, $message, $headers);
 }
 ?>
+<?php if (!isset($contact)){ ?>
 <form method="post" class="contact-form">
 	<?php wp_nonce_field('etendard_contact_nonce', 'etendard_contact_nonce'); ?>
 	
@@ -10,7 +25,7 @@ if (isset($_POST['etendard_contact_nonce'])){
 		<label for="etendard_contact_nom">
 			<?php _e('Nom'); ?>:
 		</label>
-		<input type="text" id="etendard_contact_nom" name="etendard_contact_nom" />
+		<input type="text" id="etendard_contact_nom" name="etendard_contact_nom" required />
 	</div>
 	
 	<div class="field">
@@ -31,10 +46,18 @@ if (isset($_POST['etendard_contact_nonce'])){
 		<label for="etendard_contact_message">
 			<?php _e('Message'); ?>:
 		</label>
-		<textarea id="etendard_contact_message" name="etendard_contact_message"></textarea>
+		<textarea id="etendard_contact_message" name="etendard_contact_message" required></textarea>
 	</div>
 	
 	<div class="submit">
 		<input type="submit" value="<?php _e('Envoyer'); ?>" />
 	</div>
 </form>
+<?php } else { ?>
+	<div class="contact-feedback <?php echo ($contact) ? 'success' : 'error'; ?>">
+	<?php
+	if ($contact) _e('Votre message à été envoyé.');
+	else _e("Une erreur est survenue lors de l'envoi de votre message, merci de réessayer ultérieurement.");
+	?>
+	</div>
+<?php } ?>
