4c4bbc6c5b9c994ec9f9ec2c4d828a00356f041b
Alex <alex@wp-themes-pro.com>
2014-01-29 08:44:13 +0100

    Pas besoin de commentaires sur page no sidebar

--- template-nosidebar.php
+++ template-nosidebar.php
@@ -10,7 +10,6 @@ Template Name: Sans sidebar
 			<?php /* The loop */ ?>
 			<?php while (have_posts()) : the_post(); ?>
 				<?php get_template_part('content', get_post_format()); ?>
-				<?php comments_template(); ?>
 			<?php endwhile; ?>
 		</div>
 	</div>
