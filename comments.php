61a782900b5f6e1056455050e8b481ee8fc8033f
Alex <alex@wp-themes-pro.com>
2014-01-29 08:34:21 +0100

    Fix commentaires
    J’espère que ma modif des if endif en accolades ne te dérange pas :P

--- comments.php
+++ comments.php
@@ -1,7 +1,7 @@
 <?php if (post_password_required()) return; ?>
  
 <div id="comments" class="comments-area">
-	<?php if (have_comments()) : ?>
+	<?php if (have_comments()){ ?>
 		<h2 class="comments-title">
 			<?php
                 printf(_n('Une personne à écrit un commentaire, et vous ?', '%1$s personnes ont déja écrit un commentaire, et vous ?', get_comments_number(), TEXT_TRANSLATION_DOMAIN), number_format_i18n(get_comments_number()));
@@ -20,7 +20,7 @@
 			<?php wp_list_comments(array('callback'=>'etendard_comment')); ?>
 		</ol><!-- .commentlist -->
  
-        <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
+        <?php if (get_comment_pages_count() > 1 && get_option('page_comments')){ ?>
 		<nav role="navigation" id="comment-nav-below" class="comment-navigation">
 			<div class="nav-previous">
             	<?php previous_comments_link(__('Commentaires précédents', TEXT_TRANSLATION_DOMAIN)); ?>
@@ -29,13 +29,25 @@
             	<?php next_comments_link(__('Commentaires suivants', TEXT_TRANSLATION_DOMAIN)); ?>
             </div>
         </nav><!-- #comment-nav-before .site-navigation .comment-navigation -->
-        <?php endif; // check for comment navigation ?>
+        <?php } // check for comment navigation ?>
  
-	<?php endif; // have_comments() ?>
- 
-    <?php if (!comments_open() && get_comments_number() != '0' && post_type_supports(get_post_type(), 'comments')) : ?>
+	<?php }else{  ?>
+				<h2 class="comments-title">
+					<?php __('Soyez le premier à rédiger un commentaire.', TEXT_TRANSLATION_DOMAIN); ?>
+				</h2>
+				
+				<?php comment_form(array(
+					'comment_notes_before'=>'',
+					'comment_notes_after'=>'',
+					'title_reply'=>'',
+					'title_reply_to'=>__('Répondre à %s', TEXT_TRANSLATION_DOMAIN),
+					'label_submit'=>__('Envoyer', TEXT_TRANSLATION_DOMAIN),
+				)); 
+			} ?>
+		
+    <?php if (!comments_open() && get_comments_number() != '0' && post_type_supports(get_post_type(), 'comments')){ ?>
 		<p class="nocomments">
 			<?php _e('Les commentaires sont clos.', TEXT_TRANSLATION_DOMAIN); ?>
 		</p>
-	<?php endif; ?>
+	<?php } ?>
 </div><!-- #comments .comments-area -->
