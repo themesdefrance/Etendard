61e1647402892dd522c5130eb1c3aa4399df2677
Alex <alex@wp-themes-pro.com>
2014-01-30 14:59:16 +0100

    Fix tag et cat

--- content.php
+++ content.php
@@ -1,6 +1,8 @@
 <article <?php post_class('article'); ?>>
+
 	<header class="header">
-		<?php if(is_category() && is_tag()){ ?>
+	
+		<?php if(!is_category() && !is_tag()){ ?>
 			
 			<?php if (has_post_thumbnail() && !post_password_required()){ ?>
 			
@@ -42,34 +44,48 @@
 		<?php if( !is_singular('service') && !is_page()){ ?> 
 		
 		<span class="header-meta">
+		
 			<?php echo sprintf('Publié le %2$s dans ', get_post_format_string(get_post_format()), get_the_date()); ?> 
+			
 			<?php echo sprintf('%1$s', the_category('/')); ?> |
+			
 			<?php comments_number(__('Aucun commentaire', TEXT_TRANSLATION_DOMAIN), __('Un commentaire', TEXT_TRANSLATION_DOMAIN), __('% commentaires', TEXT_TRANSLATION_DOMAIN)); ?> 
+			
 			<?php edit_post_link(__('Éditer', TEXT_TRANSLATION_DOMAIN)); ?>
+			
 		</span>
+		
 		<?php } ?>
 		
 	</header>
+	
 	<div class="content">
+	
 		<?php if (is_single() || is_page()){
+		
 				the_content();
 			
 			} else {
+			
 				the_excerpt(__('Lire la suite', TEXT_TRANSLATION_DOMAIN)); ?>
-				<a href="<?php the_permalink(); ?>" class="bouton" title="<?php the_title(); ?>">Lire la suite</a>
-				<?php
 				
-			}
-		?>
+				<a href="<?php the_permalink(); ?>" class="bouton" title="<?php the_title(); ?>">Lire la suite</a>
+				
+		<?php } ?>
+		
 	</div>
+	
 	<footer class="footer">
+	
 		<nav>
-		<?php 
-		wp_link_pages(array(
+		
+		<?php wp_link_pages(array(
 			'before'=>'<div class="page-links"><span class="page-links-title">'.__('Pages:', TEXT_TRANSLATION_DOMAIN).'</span>', 
 			'after'=>'</div>'
-		));
-		?>
+		)); ?>
+		
 		</nav>
+		
 	</footer>
+	
 </article>
