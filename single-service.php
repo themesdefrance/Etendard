d34c2f0831fcd0e9bb606f78b37836d2409929dd
Alex <alex@wp-themes-pro.com>
2014-01-30 16:20:20 +0100

    Virer les coms sur single service

--- single-service.php
+++ single-service.php
@@ -6,7 +6,6 @@
 			<?php /* The loop */ ?>
 			<?php while (have_posts()) : the_post(); ?>
 				<?php get_template_part('content', get_post_format()); ?>
-				<?php comments_template(); ?>
 			<?php endwhile; ?>
 		</div>
 		
