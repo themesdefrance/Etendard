cfab43caf9ce7f4c9843fe45e9c5511a615dd386
Yannick Lohse <glorf.travian@gmail.com>
2014-02-19 19:39:06 +0100

    Style menu fixed #97

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 </head>
 <body <?php body_class(); ?>>
 <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
-	<header class="main-header">
+	<header class="main-header sticky">
 		<div class="wrapper">
 			<?php if (of_get_option('etendard_logo')): ?>
 			<div class="logo-wrap">
