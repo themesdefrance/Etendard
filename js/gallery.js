ffe3d835e68e2a6348ae592021d5d8fd3c626e6a
Yannick Lohse <glorf.travian@gmail.com>
2014-03-02 11:08:23 +0100

    Fix  #166

--- js/gallery.js
+++ js/gallery.js
@@ -4,9 +4,7 @@ jQuery(function($){
 			galName = 'group-'+Math.round(Math.random()*1000);
 		
 		$gallery.filter('[id^="gallery"]').find('.gallery-item a').each(function(){
-			var $this = $(this);
-			$this.attr('href', $this.find('img').attr('src'));
-			$this.attr('rel', galName);
+			$(this).attr('rel', galName);
 		}).fancybox();
 	});
 });
