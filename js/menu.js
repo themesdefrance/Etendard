a504a5ac63e6aaf7c42db90cbdb3c3d9ceafef15
Yannick Lohse <glorf.travian@gmail.com>
2014-03-02 23:23:27 +0100

    Calcul taille responsove #163

--- js/menu.js
+++ js/menu.js
@@ -11,13 +11,26 @@ jQuery(function($){
 		});
 		
 		var $menu = $('.top-level-menu');
+		
 		$menu.css('max-height', '100%');
-		var menuHeight = $menu.height();
+		var menuHeight = $menu.height(),
+			menuTimeout = null;
 		
 		if (!$('#menu-toggle').is(':checked')) $menu.css('height', '0');
 		else $menu.css('height', menuHeight+'px');
 		
-		$('#menu-toggle').change(function(){
+		$(window).resize(function(){
+			if (menuTimeout) clearTimeout(menuTimeout);
+			menuTimeout = setTimeout(recalculateMenuSize, 100);
+		});
+		
+		var recalculateMenuSize = function(){
+			$menu.css('height', 'auto');
+			menuHeight = $menu.height();
+			$menu.css('height', '0');
+		}
+		
+		$('#menu-toggle').change(function(){			
 			if ($(this).is(':checked')){
 				$menu.css('height', menuHeight+'px');
 			}
