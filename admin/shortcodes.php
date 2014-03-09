c32956f539b66048a3966b5e659d380e28f2a322
Alex <alex@themesdefrance.fr>
2014-03-09 16:31:40 +0100

    Déménagement shortcodes

---
+++ admin/shortcodes.php
@@ -0,0 +1,126 @@
+<?php 
+
+// Bouton
+if (!function_exists('etendard_button')){
+	function etendard_button($atts, $content=null){
+		$autrefenetre='';
+		if(isset($atts['autrefenetre']) && $atts['autrefenetre']=='OUI'){
+			$autrefenetre='target="_blank"';
+		}
+		return '<a href="'.$atts['lien'].'" class="bouton" '.$autrefenetre.'>'.$content.'</a>';
+	}
+	add_shortcode('bouton', 'etendard_button');
+}
+
+// Colonnes
+if (!function_exists('etendard_shortcode_colonne')){
+	function etendard_shortcode_colonne($class, $atts, $content){
+		$premier = (isset($atts[0]) && trim($atts[0]) == 'premier') ? ' premier' : '';
+		$res = '<div class="'.$class.$premier.'">';
+		$res .= do_shortcode(wpautop($content));
+		$res .= '</div>';
+		return $res;
+	}
+}
+
+// 1/2
+if (!function_exists('etendard_un_demi')){
+	function etendard_un_demi($atts, $content=null){
+		return etendard_shortcode_colonne('un_demi', $atts, $content);
+	}
+	add_shortcode('un_demi', 'etendard_un_demi');
+}
+
+// 1/3
+if (!function_exists('etendard_un_tiers')){
+	function etendard_un_tiers($atts, $content=null){
+		return etendard_shortcode_colonne('un_tiers', $atts, $content);
+	}
+	add_shortcode('un_tiers', 'etendard_un_tiers');
+}
+
+// 1/4
+if (!function_exists('etendard_un_quart')){
+	function etendard_un_quart($atts, $content=null){
+		return etendard_shortcode_colonne('un_quart', $atts, $content);
+	}
+	add_shortcode('un_quart', 'etendard_un_quart');
+}
+
+// 2/3
+if (!function_exists('etendard_deux_tiers')){
+	function etendard_deux_tiers($atts, $content=null){
+		return etendard_shortcode_colonne('deux_tiers', $atts, $content);
+	}
+	add_shortcode('deux_tiers', 'etendard_deux_tiers');
+}
+
+// 3/4
+if (!function_exists('etendard_trois_quarts')){
+	function etendard_trois_quarts($atts, $content=null){
+		return etendard_shortcode_colonne('trois_quarts', $atts, $content);
+	}
+	add_shortcode('trois_quarts', 'etendard_trois_quarts');
+}
+
+// Messages
+// Info
+if (!function_exists('etendard_message_info')){
+	function etendard_message_info($atts, $content=null){
+		$res = '<div class="message info">';
+		$res.= wpautop($content);
+		$res.= '</div>';
+		return $res;
+	}
+	add_shortcode( 'info', 'etendard_message_info' );
+}
+
+// Alerte
+if (!function_exists('etendard_message')){
+	function etendard_message($class, $content){
+		$res = '<div class="message '.$class.'">';
+		$res .= wpautop($content);
+		$res .= '</div>';
+		return $res;
+	}
+}
+
+if (!function_exists('etendard_message_alerte')){
+	function etendard_message_alerte($atts, $content=null){
+		return etendard_message('alerte', $content);
+	}
+	add_shortcode('alerte', 'etendard_message_alerte');
+}
+
+// Erreur
+if (!function_exists('etendard_message_erreur')){
+	function etendard_message_erreur($atts, $content=null){
+		return etendard_message('erreur', $content);
+	}
+	add_shortcode('erreur', 'etendard_message_erreur');
+}
+
+// Succès
+if (!function_exists('etendard_message_succes')){
+	function etendard_message_succes($atts, $content=null){
+		return etendard_message('succes', $content);
+	}
+	add_shortcode('succes', 'etendard_message_succes');
+}
+
+// Appel à l'action
+// En largeur
+if (!function_exists('etendard_appel_action')){
+	function etendard_appel_action($atts, $content=null){
+		return '<div class="embedcta">
+						<p class="cta-text">
+							'.$content.'
+						</p>
+						<div class="button-wrapper">
+							<a href="'.$atts['lien'].'" class="cta-button">'.$atts['bouton'].'</a>
+						</div>
+				</div>';
+	}
+	add_shortcode('appel_action', 'etendard_appel_action');
+}
+
--- functions.php
+++ functions.php
@@ -8,7 +8,8 @@ define('EDD_SL_LICENSE_KEY', 'etendard_license_edd');
 
 define('COCORICO_PREFIX', 'etendard_');
 
-require_once 'admin/Cocorico/cocorico.php';
+$cocoricofile = get_stylesheet_directory_uri() . 'admin/Cocorico/Cocorico.php';
+
 
 //chargement des widgets	
 require_once 'admin/widgets/social.php';
@@ -60,14 +61,9 @@ if (!function_exists('etendard_setup')){
 		
 		add_theme_support('post-thumbnails');
 		
-//		add_theme_support('post-formats', array(
-////			'chat', 
-//			'image', 
-//			'link', 
-//			'quote', 
-////			'status', 
-//			'video',
-//		));
+		add_theme_support('post-formats', array(
+			'video'
+		));
 		
 		add_image_size('etendard-portfolio-thumbnail', 301, 230, true);
 		add_image_size('etendard-service-thumbnail', 230, 230, true);
@@ -292,6 +288,10 @@ if (!function_exists('etendard_strip_img_sizes')){
 add_filter('get_avatar', 'etendard_strip_img_sizes');
 add_filter('post_thumbnail_html', 'etendard_strip_img_sizes');
 
+// Chargement des shortcodes
+require_once 'admin/shortcodes.php';
+
+
 //fonction de pagination
 if (!function_exists('etendard_posts_nav')){
 	//derived from http://www.wpbeginner.com/wp-themes/how-to-add-numeric-pagination-in-your-wordpress-theme/
@@ -477,134 +477,6 @@ function etendard_is_paginated_post() {
  
 }
 
-
-////////////////////////////////////
-// Shortcodes
-////////////////////////////////////
-
-// Bouton
-if (!function_exists('etendard_button')){
-	function etendard_button($atts, $content=null){
-		$autrefenetre='';
-		if(isset($atts['autrefenetre']) && $atts['autrefenetre']=='OUI'){
-			$autrefenetre='target="_blank"';
-		}
-		return '<a href="'.$atts['lien'].'" class="bouton" '.$autrefenetre.'>'.$content.'</a>';
-	}
-	add_shortcode('bouton', 'etendard_button');
-}
-
-// Colonnes
-if (!function_exists('etendard_shortcode_colonne')){
-	function etendard_shortcode_colonne($class, $atts, $content){
-		$premier = (isset($atts[0]) && trim($atts[0]) == 'premier') ? ' premier' : '';
-		$res = '<div class="'.$class.$premier.'">';
-		$res .= do_shortcode(wpautop($content));
-		$res .= '</div>';
-		return $res;
-	}
-}
-
-// 1/2
-if (!function_exists('etendard_un_demi')){
-	function etendard_un_demi($atts, $content=null){
-		return etendard_shortcode_colonne('un_demi', $atts, $content);
-	}
-	add_shortcode('un_demi', 'etendard_un_demi');
-}
-
-// 1/3
-if (!function_exists('etendard_un_tiers')){
-	function etendard_un_tiers($atts, $content=null){
-		return etendard_shortcode_colonne('un_tiers', $atts, $content);
-	}
-	add_shortcode('un_tiers', 'etendard_un_tiers');
-}
-
-// 1/4
-if (!function_exists('etendard_un_quart')){
-	function etendard_un_quart($atts, $content=null){
-		return etendard_shortcode_colonne('un_quart', $atts, $content);
-	}
-	add_shortcode('un_quart', 'etendard_un_quart');
-}
-
-// 2/3
-if (!function_exists('etendard_deux_tiers')){
-	function etendard_deux_tiers($atts, $content=null){
-		return etendard_shortcode_colonne('deux_tiers', $atts, $content);
-	}
-	add_shortcode('deux_tiers', 'etendard_deux_tiers');
-}
-
-// 3/4
-if (!function_exists('etendard_trois_quarts')){
-	function etendard_trois_quarts($atts, $content=null){
-		return etendard_shortcode_colonne('trois_quarts', $atts, $content);
-	}
-	add_shortcode('trois_quarts', 'etendard_trois_quarts');
-}
-
-// Messages
-// Info
-if (!function_exists('etendard_message_info')){
-	function etendard_message_info($atts, $content=null){
-		$res = '<div class="message info">';
-		$res.= wpautop($content);
-		$res.= '</div>';
-		return $res;
-	}
-	add_shortcode( 'info', 'etendard_message_info' );
-}
-
-// Alerte
-if (!function_exists('etendard_message')){
-	function etendard_message($class, $content){
-		$res = '<div class="message '.$class.'">';
-		$res .= wpautop($content);
-		$res .= '</div>';
-		return $res;
-	}
-}
-
-if (!function_exists('etendard_message_alerte')){
-	function etendard_message_alerte($atts, $content=null){
-		return etendard_message('alerte', $content);
-	}
-	add_shortcode('alerte', 'etendard_message_alerte');
-}
-
-// Erreur
-if (!function_exists('etendard_message_erreur')){
-	function etendard_message_erreur($atts, $content=null){
-		return etendard_message('erreur', $content);
-	}
-	add_shortcode('erreur', 'etendard_message_erreur');
-}
-
-// Succès
-if (!function_exists('etendard_message_succes')){
-	function etendard_message_succes($atts, $content=null){
-		return etendard_message('succes', $content);
-	}
-	add_shortcode('succes', 'etendard_message_succes');
-}
-
-// Appel à l'action
-// En largeur
-if (!function_exists('etendard_appel_action')){
-	function etendard_appel_action($atts, $content=null){
-		return '<div class="embedcta">
-						<p class="cta-text">
-							'.$content.'
-						</p>
-						<div class="button-wrapper">
-							<a href="'.$atts['lien'].'" class="cta-button">'.$atts['bouton'].'</a>
-						</div>
-				</div>';
-	}
-	add_shortcode('appel_action', 'etendard_appel_action');
-}
 
 
 ////////////////////////////////////
