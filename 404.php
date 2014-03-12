67660cb7ec6e5fa24950e386bee88711accbb12c
Yannick Lohse <glorf.travian@gmail.com>
2014-03-12 19:46:58 +0100

    Ajustement #117

--- 404.php
+++ 404.php
@@ -5,9 +5,7 @@
 			<?php _e('Oups, vous voici dans une impasse…', TEXT_TRANSLATION_DOMAIN); ?>
 		</h1>
 		<div>
-			<p>
-				<img src="<?php echo get_stylesheet_directory_uri()."/img/triste.png" ?>" alt="<?php _e('Erreur 404', TEXT_TRANSLATION_DOMAIN); ?>">
-			</p>
+			<img src="<?php echo get_stylesheet_directory_uri()."/img/triste.png" ?>" alt="<?php _e('Erreur 404', TEXT_TRANSLATION_DOMAIN); ?>" class="smiley-404">
 			<p>
 				<?php printf(__("La page demandée n'a pas été trouvée. <a href=\"%s\">Cliquez ici</a> retourner sur la page d'accueil.", TEXT_TRANSLATION_DOMAIN), home_url()); ?>
 			</p>
