bde40f01b8dd02a84dcf07cf226841a9d37e001e
Yannick Lohse <glorf.travian@gmail.com>
2014-03-19 19:53:55 +0100

    Fix bugs js admin diaporama

--- admin/Cocorico/extensions/etendard/etendard.js
+++ admin/Cocorico/extensions/etendard/etendard.js
@@ -3,16 +3,18 @@
 		$('.etendard-diaporama-add').click(function(){
 			var $table = $(this).prev();
 			
-			var $line = $table.find('tr').last().clone().appendTo($table);
+			var $lines = $table.find('tr');
+			$lines = $lines.slice($lines.length-3);
+			var $newLines = $lines.clone().appendTo($table);
 			
-			$line.find('input[type="text"]').val('');
-			$line.find('.cocorico-preview-wrapper').hide();
-			//@TODO: set new ids
+			$newLines.find('input[type="text"]').val('');
+			$newLines.find('.cocorico-preview-wrapper').hide();
 		});
 		
 		$('.etendard-delete-diaporama').live('click', function(event){
 			event.preventDefault();
-			$(this).parentsUntil('tr').parent().remove();
+			var $row = $(this).parentsUntil('tr').parent();
+			$row.add($row.prev()).add($row.prev().prev()).remove();
 		});
 	});
 })(jQuery);
