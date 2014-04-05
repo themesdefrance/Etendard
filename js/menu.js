1476429e3bd6b4cf5a67fba8f19e10cc231a7844
Yannick Lohse <glorf.travian@gmail.com>
2014-04-05 22:51:25 +0200

    Fix #234

--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,10 @@ jQuery(function($){
 	$(function(){
 		var stickyClass = 'sticky',
 			isSticked = false,
-			offsetTrigger = 0;
+			offsetTrigger = 5;
 		
 		$(window).on('scroll', function(){
-			isSticked = ($('.main-header.'+stickyClass).length > 0) ? true : false;
+			isSticked = ($('.main-header.'+stickyClass).length > 1) ? true : false;
 			if ($(window).scrollTop() > offsetTrigger && !isSticked) $('.main-header').addClass(stickyClass);
 			else if ($(window).scrollTop() <= offsetTrigger && isSticked) $('.main-header').removeClass(stickyClass);
 		});
