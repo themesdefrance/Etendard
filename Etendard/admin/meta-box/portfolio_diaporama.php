754bdca2c7af8f6945a60a209f105762e28ebb50
Yannick Lohse <glorf.travian@gmail.com>
2014-04-12 11:38:22 +0200

    Suppression du dossier de build

--- Etendard/admin/meta-box/portfolio_diaporama.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$cocorico = new Cocorico(false);
-$cocorico->diaporama('portfolio_diaporama');
-//$cocorico->component('etendard-diaporama', 'portfolio_diaporama')->filter('diaporama-save');
-$cocorico->render();
--- Etendard/home_elements/diaporama.php
+++
@@ -1,3 +0,0 @@
-<section class="diaporama">
-	<?php get_template_part('diaporama'); ?>
-</section>
