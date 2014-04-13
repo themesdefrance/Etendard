68c947aa4cba00c95af9e8d3602e5384c2006720
Yannick Lohse <glorf.travian@gmail.com>
2014-04-13 18:56:06 +0200

    Update gitignore

---
+++ admin/Cocorico/extensions/etendard/ordre.js
@@ -0,0 +1,16 @@
+(function($){
+	$(function(){
+		$('ul.slip-target').each(function(){
+			new Slip(this);
+			
+			this.addEventListener('slip:reorder', function(e){
+				e.target.parentNode.insertBefore(e.target, e.detail.insertBefore);
+				return false;
+			}, false);
+			
+			this.addEventListener('slip:beforeswipe', function(e){
+				e.preventDefault();
+			}, false);
+		});
+	});
+})(jQuery);
