974ce2ab83075d2f5552dbabd40fdd1cf2551233
Alex <alex@wp-themes-pro.com>
2014-04-14 14:44:08 +0200

    Lien auteur dans meta article

--- content.php
+++ content.php
@@ -50,9 +50,7 @@
 		
 		<span class="header-meta">
 		
-			<?php echo sprintf('Publié le %2$s dans ', get_post_format_string(get_post_format()), get_the_date()); ?> 
-			
-			<?php echo sprintf('%1$s', the_category('/')); ?> |
+			<?php echo sprintf(__('Publié le %2$s par %3$s dans %4$s | '), get_post_format_string(get_post_format()), get_the_date(), get_the_author_link(), get_the_category_list('/')); ?> 
 			
 			<?php comments_number(__('Aucun commentaire', TEXT_TRANSLATION_DOMAIN), __('Un commentaire', TEXT_TRANSLATION_DOMAIN), __('% commentaires', TEXT_TRANSLATION_DOMAIN)); ?> 
 			
