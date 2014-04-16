107f1550d9d8d7f4db5ff2e73a6c9fae51fd2291
Alex <alex@wp-themes-pro.com>
2014-04-16 09:45:02 +0200

    Affichage légende images lightbox

--- js/gallery.js
+++ js/gallery.js
@@ -5,10 +5,20 @@ jQuery(function($){
 		
 		$gallery.filter('[id^="gallery"]').find('.gallery-item a').each(function(){
 			$(this).attr('rel', galName);
+			$(this).attr('title', this.title);
 		}).fancybox();
 	});
 	
 	$('article.post.type-post').find('a > img').each(function(){
+		if($(this).parent().attr('title')== undefined){
+			if($(this).attr('title') != undefined){
+				$(this).parent().attr('title', $(this).attr('title'));
+			}else{
+				$(this).parent().attr('title', $(this).attr('alt'));
+			}
+		}
 		$(this).parent().fancybox();
 	});
+
 });
+
