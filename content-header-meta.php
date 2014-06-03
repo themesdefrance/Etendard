dbfe9771e484b2f4c261da3aca4705717f1d855e
Alex <alex@wp-themes-pro.com>
2014-06-03 16:52:15 +0200

    Lien vers la page auteur dans header meta

--- content-header-meta.php
+++ content-header-meta.php
@@ -2,7 +2,7 @@
 		
 	<span class="header-meta">
 	
-		<?php echo sprintf(__('Publié le %2$s par %3$s dans %4$s | '), get_post_format_string(get_post_format()), get_the_date(), get_the_author_link(), get_the_category_list('/')); ?> 
+		<?php echo sprintf(__('Publié le %2$s par <a href="%3$s">%4$s</a> dans %5$s | ','etendard'), get_post_format_string(get_post_format()), get_the_date(), get_author_posts_url( get_the_author_meta( 'ID' ) ), get_the_author_meta('display_name') ,get_the_category_list('/')); ?> 
 		
 		<?php comments_number(__('Aucun commentaire', 'etendard'), __('Un commentaire', 'etendard'), __('% commentaires', 'etendard')); ?> 
 		
