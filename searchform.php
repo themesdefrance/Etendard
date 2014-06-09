984dc3a4a4b54bb4c1f1e20556a12e622394a80d
Alex <alex@wp-themes-pro.com>
2014-06-09 08:14:03 +0200

    Correction searchform.php

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<label>
 		<span class="screen-reader-text">
-			<?php _e('Rerchercher :', 'etendard'); ?>
+			<?php _e('Rechercher :', 'etendard'); ?>
 		</span>
 		<input class="search-field" placeholder="<?php _e('Recherche…', 'etendard'); ?>" value="" name="s" title="<?php _e('Recherche…', 'etendard'); ?>" type="search" />
 	</label>
