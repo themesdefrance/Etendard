660e3c6bbcf58d9f6a4f1730e6aaf27611211ec6
Alex <alex@wp-themes-pro.com>
2014-07-03 12:50:55 +0200

    Fix 404 theme enfant

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@
 			<?php _e('Oups, vous voici dans une impasse…', 'etendard'); ?>
 		</h1>
 		<div class="content">
-			<img src="<?php echo get_stylesheet_directory_uri()."/img/triste.png" ?>" alt="<?php _e('Erreur 404', 'etendard'); ?>" class="smiley-404">
+			<img src="<?php echo get_template_directory_uri()."/img/triste.png" ?>" alt="<?php _e('Erreur 404', 'etendard'); ?>" class="smiley-404">
 			<p>
 				<?php printf(__("La page demandée n'a pas été trouvée. <a href=\"%s\">Cliquez ici</a> retourner sur la page d'accueil.", 'etendard'), home_url()); ?>
 			</p>
