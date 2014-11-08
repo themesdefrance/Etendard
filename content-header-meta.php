6c90ec260a6309f0a4f3094dfcc4a830bc595fba
alex_borto <themesdefrance@users.noreply.github.com>
2014-11-08 13:54:21 +0100

    Update content header meta

--- content-header-meta.php
+++ content-header-meta.php
@@ -1,12 +1,38 @@
+<?php
+	$post_header_date     = apply_filters('etendard_post_header_date', true);
+	$post_header_author   = apply_filters('etendard_post_header_author', true);
+	$post_header_category = apply_filters('etendard_post_header_category', true);
+	$post_header_comments = apply_filters('etendard_post_header_comments', true);
+?>
+
 <?php if(!is_page() && !is_tag()): ?> 
 		
 	<span class="header-meta">
 	
-		<?php echo sprintf(__('Published on %2$s by <a href="%3$s">%4$s</a> in %5$s | ','etendard'), get_post_format_string(get_post_format()), get_the_date(), get_author_posts_url( get_the_author_meta( 'ID' ) ), get_the_author_meta('display_name') ,get_the_category_list('/')); ?> 
+		<?php
 		
-		<?php comments_number(__('No Comment', 'etendard'), __('One Comment', 'etendard'), __('% Comments', 'etendard')); ?> 
+		if($post_header_date || $post_header_author || $post_header_category){
+			_e('Published ','etendard');
+		}
+		if($post_header_date){
+			echo sprintf(__('on %s ','etendard'),get_the_date());
+		}
+		if($post_header_author){
+			echo sprintf(__('by <a href="%s">%s</a> ','etendard'), get_author_posts_url(get_the_author_meta('ID')), get_the_author_meta('display_name') );
+		}
+		if($post_header_category){
+			echo sprintf(__('in %s ','etendard'), get_the_category_list('/'));
+		}
+		if($post_header_date || $post_header_author || $post_header_category){
+			echo '| ';
+		}
+		if($post_header_comments){
+			comments_number(__('No Comment', 'etendard'), __('One Comment', 'etendard'), __('% Comments', 'etendard'));
+			echo ' | ';
+		}
 		
-		<?php edit_post_link(__('Edit', 'etendard')); ?>
+		edit_post_link(__('Edit', 'etendard'));
+	?>
 		
 	</span>
 
