b73f9a6493f1c23839bc9d126a0503f58d5c8557
Alex Borto <alex@themesdefrance.fr>
2015-04-12 11:24:07 +0200

    Fix commentaires

--- single.php
+++ single.php
@@ -5,47 +5,47 @@
 <?php get_template_part('header-bar'); ?>
 
 <section class="blog">
-	
+
 	<div class="wrapper">
-		
-		<?php do_action('etendard_before_main'); ?>	
-		
+
+		<?php do_action('etendard_before_main'); ?>
+
 		<div class="layout-grid">
-			
+
 			<?php do_action('etendard_top_main'); ?>
-			
+
 			<?php if ($position === 'gauche')get_sidebar('blog'); ?>
 
 			<div class="col-2-3 <?php if (get_option('etendard_sidebar_position') === 'sans') echo 'landing' ?>" role="main" itemprop="mainContentOfPage">
-				
+
 				<?php do_action('etendard_top_main'); ?>
-				
+
 				<ul class="articles">
 					<?php while (have_posts()) : the_post(); ?>
 					<li>
-						<?php 
+						<?php
 							get_template_part('content', get_post_format());
-							
+
 							etendard_single_post_nav();
-							
-							comments_template();
+
+							if(comments_open()) comments_template();
 						?>
 					</li>
 					<?php endwhile; ?>
 				</ul>
-				
+
 			</div><!--END .col-2-3-->
 
 			<?php if ($position === 'droite' || !$position)get_sidebar('blog'); ?>
-			
+
 			<?php do_action('etendard_bottom_main'); ?>
-			
+
 		</div><!--END .layout-grid-->
-		
+
 		<?php do_action('etendard_after_main'); ?>
-		
+
 	</div><!--END .wrapper-->
-	
+
 </section><!--END .blog-->
 
 <?php get_footer(); ?>
